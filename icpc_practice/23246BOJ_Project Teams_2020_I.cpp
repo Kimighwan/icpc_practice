b8bacd1e0b1457e993107d3e7a643175c23e7a92
Kimikhwan <moonfgg@naver.com>
2024-10-18 19:10:04 +0900

    10.18

--- icpc_practice/23246BOJ_Project Teams_2020_I.cpp
+++ icpc_practice/23246BOJ_Project Teams_2020_I.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int n;
 int w[100'002];
+int mn = 222222;
+pair<int, int> p[5'001];
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -12,5 +15,13 @@ int main() {
 	for (int i = 0; i < 2 * n; i++)
 		cin >> w[i];
 
+	sort(w, w + (2 * n));
 
+	for (int i = 0; i < n; i++)
+	{
+		p[i] = { w[i], w[2 * n - 1 - i] };
+		mn = min(p[i].first + p[i].second, mn);
+	}
+
+	cout << mn;
 }
